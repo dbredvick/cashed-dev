83f55938de3d0026cc4041a897856dc46df7267a
Drew Bredvick <dbredvick@gmail.com>
2023-04-14 10:51:51 -0500

    ogStuff

--- src/app/(main-player)/episodes.jsx
+++ src/app/(main-player)/episodes.jsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useMemo } from 'react'
-import Head from 'next/head'
 import Link from 'next/link'
 import slugify from '@/lib/slugify'
 
@@ -101,16 +100,6 @@ function EpisodeEntry({ episode }) {
 export default function Home({ episodes }) {
     return (
         <>
-            <Head>
-                <title>
-                    Cashed.dev - Conversations with the most tragically misunderstood
-                    people of our time
-                </title>
-                <meta
-                    name="description"
-                    content="Conversations with the most tragically misunderstood people of our time."
-                />
-            </Head>
             <div className="pb-12 pt-16 sm:pb-4 lg:pt-12">
                 <Container>
                     <h1 className="text-2xl font-bold leading-7 text-slate-900">
@@ -119,7 +108,7 @@ export default function Home({ episodes }) {
                 </Container>
                 <div className="divide-y divide-slate-100 sm:mt-4 lg:mt-8 lg:border-t lg:border-slate-100">
                     {episodes.map((episode) => (
-                        <EpisodeEntry key={episode.id} episode={episode} />
+                        <EpisodeEntry key={slugify(episode.title)} episode={episode} />
                     ))}
                 </div>
             </div>
