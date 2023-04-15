7c072c6e057454108658f1c7528be6c3fa531796
Drew Bredvick <dbredvick@gmail.com>
2023-04-15 10:53:45 -0500

    testNode

--- src/app/(main-player)/[slug]/opengraph-image.jsx
+++ src/app/(main-player)/[slug]/opengraph-image.jsx
@@ -6,7 +6,6 @@ export const size = { width: 1200, height: 600 };
 export const alt = 'About this episode of Cashed.dev';
 export const contentType = 'image/png';
 
-export const runtime = 'edge'
 
 function randomBetween(min, max, seed = 1) {
     return () => {
